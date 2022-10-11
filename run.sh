b9aecc1283aa9e8341b033b66d50e36f32b1e13c
Jonas 'Saitama' Schwindt <jonas.schwindt@impact-z.one>
2022-10-12 00:31:10 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /opt/impactzone/impactzone-website
-sass public/css/iz-styles.scss public/css/iz-styles.css
-(npm run dev &) &
+#sass public/css/iz-styles.scss public/css/iz-styles.css
+(npm run start &) &
